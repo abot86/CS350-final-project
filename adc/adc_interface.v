e5f79c852b799f9a3fd4c27635e465f968ad9d14
Ishan Vyas <isv4@duke.edu>
2024-11-16 15:35:42 -0500

    adc interface

--- adc/adc_interface.v
+++ adc/adc_interface.v
@@ -1,5 +1,13 @@
 module adc_interface(
     input CLK100MHZ;
+    input reset,          // Reset signal
+    input eoc,            // End-of-conversion signal from ADC0808
+    input [7:0] data_in,  // Digital output from ADC0808
+    output reg ale,       // Address latch enable for ADC0808
+    output reg start,     // Start conversion signal for ADC0808
+    output reg oe,        // Output enable signal for ADC0808
+    output reg [2:0] addr,// Address lines to select analog input channel
+    output reg [7:0] data_out // Digital data captured from ADC
 
 );
 
