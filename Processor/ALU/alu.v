2662dfee176a848e45ed6976a50d61b2d8c9a692
Ayush Botke <ab973@duke.edu>
2024-11-17 16:03:41 -0500

    Garbage

--- Processor/ALU/alu.v
+++ Processor/ALU/alu.v
@@ -4,8 +4,9 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
     input [4:0] ctrl_ALUopcode, ctrl_shiftamt;
 
     output [31:0] data_result;
-    output isNotEqual, isLessThan, overflow, OVF;
-
+    output isNotEqual, isLessThan, overflow;
+    
+    wire OVF;
     wire [31:0] notB, inB;
 
     wire [31:0] and_result, or_result, sll_result, sra_result, addsub_result;
@@ -47,5 +48,6 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
     not32 NOTB(notB, data_operandB);
     assign inB = ctrl_ALUopcode[0] ?  notB : data_operandB;
     adder ADDSUB(data_operandA, inB, ctrl_ALUopcode[0], addsub_result, OVF);
+    
 
 endmodule
