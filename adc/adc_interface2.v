1340c2fe173da298bd63df20bd7ca9a5f4a8c3f2
Ayush Botke <ab973@duke.edu>
2024-11-18 16:46:41 -0500

    Creating ADC interface

---
+++ adc/adc_interface2.v
@@ -0,0 +1,11 @@
+module adc_interface(
+    input wire CLK25MHZ,
+    input reset,          // Reset signal
+    input [7:0] JA,  // Digital output from ADC0808
+    output reg [7:0] LED // Digital data captured from ADC
+);
+
+    
+
+endmodule
+
