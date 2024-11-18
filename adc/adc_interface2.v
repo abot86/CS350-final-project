03b79087b388b9c268bc2ebd05eb818ede815ba0
Ayush Botke <ab973@duke.edu>
2024-11-18 16:56:09 -0500

    ADC to LED

--- adc/adc_interface2.v
+++ adc/adc_interface2.v
@@ -1,11 +1,9 @@
 module adc_interface(
     input wire CLK25MHZ,
-    input reset,          // Reset signal
     input [7:0] JA,  // Digital output from ADC0808
     output reg [7:0] LED // Digital data captured from ADC
 );
-
-    
+    assign JA[7:0] = LED[7:0];
 
 endmodule
 
