3f61e35e978ce22a9e9ccad4e3464f6ddb7a9e26
Ishan Vyas <isv4@duke.edu>
2024-11-17 22:21:19 -0500

    adc interface

--- adc/adc_interface.v
+++ adc/adc_interface.v
@@ -39,26 +39,11 @@ module adc_interface(
                     end
                 end
                 READ_DATA: begin
-                    data_out <= data_in;
                     state <= IDLE;
                 end
             endcase
         end
+        data_out <= data_in;
     end
 endmodule
 
-/*
-CTRL SIGNALS:
-    INPUT (OUT FROM ADC):
-    - [2^-1 : 2^-8]
-
-    OUTPUT (TO ADC):
-    - ALE (Address Latch Enable)
-
-    OTHER:
-    - ADD A / ADD B / ADD C (address selection)
-        -  just tie all to ground and keep our input on In0.
-    
-https://www.ti.com/lit/ds/symlink/adc0808-n.pdf?ts=1731677092610&ref_url=https%253A%252F%252Fwww.google.com%252F
-
-*/
