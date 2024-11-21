0e9cf038d9b6794ac810b17c2cb555dd6340f6ab
Ishan Vyas <isv4@duke.edu>
2024-11-21 12:57:10 -0500

    pwm works. for future ref, add PLL clock

--- pwm/ServoDriver.v
+++ pwm/ServoDriver.v
@@ -1,26 +1,39 @@
 module ServoDriver(
-    input CLK25MHZ,              // 100 MHz system clock
-    input reset,            // Reset signal
-    input SW[0],
+    input CLK100MHZ,              // 100 MHz system clock
+    input BTNC,            // Reset signal
+    input BTNU,
     output servoSignal     // PWM signal for the servo
     );
 
-    wire [9:0] duty_cycle, // Duty cycle input (0-1023, scaled to 0-100%)
-    always @(posedge CLK25MHZ) begin
-        if (SW[0] == 0) begin
+    wire clk25mhz;
+    wire clk;
+    wire locked;
+    
+    assign clk = clk25mhz;
+    
+    // PLL Clocking - REDO PLL LATER, NOT PUSHED TO GIT
+    clk_wiz_0 PLL(
+        .clk_out1(clk25mhz),
+        .reset(BTNC),
+        .locked(locked),
+        .clk_in1(CLK100MHZ));
+
+    reg [9:0] duty_cycle; // Duty cycle input (0-1023, scaled to 0-100%)
+    always @(posedge clk) begin
+        if (BTNU == 0) begin
             duty_cycle <= 9'd21;
         end
-        else if (SW[0] == 0) begin
+        else if (BTNU == 1) begin
             duty_cycle <= 9'd102;
         end
     end
     // Instantiate the PWMSerializer
     PWMSerializer #(
         .PERIOD_WIDTH_NS(20_000_000), // 20 ms period for servo
-        .SYS_FREQ_MHZ(25)            // 100 MHz system clock
+        .SYS_FREQ_MHZ(25)            // 25 MHz system clock
     ) pwm_gen (
-        .clk(CLK25MHZ),                    // Connect system clock
-        .reset(reset),                // Connect reset signal
+        .clk(clk),                    // Connect system clock
+        .reset(BTNC),                // Connect reset signal
         .duty_cycle(duty_cycle),      // Pass in duty cycle
         .signal(servoSignal)         // Connect to output PWM signal
     );
