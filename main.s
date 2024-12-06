4c497fac49229b3c3c5cbc5aa8ca69430b423e48
Ishan Vyas <isv4@duke.edu>
2024-12-06 11:20:30 -0500

    constants for main.s

--- main.s
+++ main.s
@@ -31,7 +31,7 @@ addi $r20, $r0, 102
 addi $r21, $r0, 21
 
 # Calibrate rest (minimum)
-addi $r9, $r0, ADC_SAMPLE                 # count = 2500
+add $r9, $r0, $r19                 # count = 2500
 Wait_btnl:
     bne $r5, $r0, Min_cal
     j Wait_btnl
@@ -44,21 +44,20 @@ Min_cal:
         bne $r8, $r0, Min_calc      # while ADC not ready
         j Min_wait_ADC
     Min_calc:
-        blt $r1, OFFSET, Min_abs    # if ($r1 > OFFSET)
-        sub $r11, $r1, OFFSET       # toAdd = $r1 - OFFSET
+        blt $r1, $r18, Min_abs    # if ($r1 > OFFSET)
+        sub $r11, $r1, $r18       # toAdd = $r1 - OFFSET
     Min_abs:
-        sub $r11, OFFSET, $r1       # toAdd = OFFSET - $r1
+        sub $r11, $r18, $r1       # toAdd = OFFSET - $r1
 
     add $r10, $r10, $r11            # sum = sum + toAdd
     j Min_cal
 
 Min_set:
-    div $r3, $r10, ADC_SAMPLE
-
+    div $r3, $r10, $r19
 
 
 # Calibrate active (maximum)
-addi $r9, $r0, ADC_SAMPLE                 # count = 2500
+addi $r9, $r0, $r19                 # count = 2500
 Wait_btnr:
     bne $r6, $r0, Max_cal
     j Wait_btnr
@@ -71,21 +70,21 @@ Max_cal:
         bne $r8, $r0, Max_calc      # while ADC not ready
         j Max_wait_ADC
     Max_calc:
-        blt $r1, OFFSET, Max_abs    # if ($r1 > OFFSET)
-        sub $r11, $r1, OFFSET       # toAdd = $r1 - OFFSET
+        blt $r1, $r18, Max_abs    # if ($r1 > OFFSET)
+        sub $r11, $r1, $r18       # toAdd = $r1 - OFFSET
     Max_abs:
-        sub $r11, OFFSET, $r1       # toAdd = OFFSET - $r1
+        sub $r11, $r18, $r1       # toAdd = OFFSET - $r1
 
     add $r10, $r10, $r11            # sum = sum + toAdd
     j Max_cal
 
 Max_set:
-    div $r4, $r10, ADC_SAMPLE
+    div $r4, $r10, $r19
 
 
 # Main loop
 Main_loop:
-    addi $r9, $r0, ADC_SAMPLE           # count = 2500
+    addi $r9, $r0, $r19           # count = 2500
 
     Data_collect:
         addi $r9, $r9, -1               # count = count - 1
@@ -95,26 +94,26 @@ Main_loop:
             bne $r8, $r0, Calc          # while ADC not ready
             j Wait_ADC
         Calc:
-            blt $r1, OFFSET, Abs        # if ($r1 > OFFSET)
-            sub $r11, $r1, OFFSET       # toAdd = $r1 - OFFSET
+            blt $r1, $r18, Abs        # if ($r1 > OFFSET)
+            sub $r11, $r1, $r18       # toAdd = $r1 - OFFSET
         Abs:
-            sub $r11, OFFSET, $r1       # toAdd = OFFSET - $r1
+            sub $r11, $r18, $r1       # toAdd = OFFSET - $r1
 
         add $r10, $r10, $r11            # sum = sum + toAdd
         j Data_collect
     
     Set_pwm:
-        div $r12, $r10, ADC_SAMPLE      # r12 := measured average over 3 periods
+        div $r12, $r10, $r19      # r12 := measured average over 3 periods
         sub $r16, $r12, $r3             # $r16 = x - rest
         sub $r17, $r4, $r12             # $r17 = active - x
         blt $r17, $r16, Set_pwm_HIGH    # if (x-rest > active-x)
 
         Set_pwm_LOW:
-            addi $r2, $r0, PWM_LOW      # set PWM register to low (0*)
+            addi $r2, $r0, $r21      # set PWM register to low (0*)
             j Main_loop
 
         Set_pwm_HIGH:
-            addi $r2, $r0, PWM_HIGH     # set PWM register to high (0*)
+            addi $r2, $r0, $r20     # set PWM register to high (0*)
             j Main_loop
     
     j Main_loop
