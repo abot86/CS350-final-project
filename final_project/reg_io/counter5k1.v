35ed8875f4af465d3b412672e67c4d651ad2ea2a
Ishan Vyas <isv4@duke.edu>
2024-12-05 20:25:55 -0500

    more test

--- final_project/reg_io/counter5k1.v
+++ final_project/reg_io/counter5k1.v
@@ -3,12 +3,12 @@ module counter5k1(
     output reg pulse5khz
 );
 
-    // counter only stays on for 1 cycle - have it stay on for longer? 
+    // counter only stays on for 1 cycle - should i have it stay on for longer? 
     reg[12:0] counter = 0;
     always @(posedge clk25mhz) begin
         if (counter < CounterLimit) begin
             counter <= counter + 1;
-            pulse5khz <= 1'b0;
+            pulse5khz <= 0;
         end
         else begin
             pulse5khz <= 1;
