b3c42c082e5c8fa5a7a97d9feee695c86a3bc62a
ishansvyas <120070801+ishansvyas@users.noreply.github.com>
2024-12-06 00:01:41 -0500

    note to ayush

--- main.s
+++ main.s
@@ -99,6 +99,7 @@ Main_loop:
 #    PLAN FOR TESTING
 # register 24 is (or will be depending on how late I stay up)...
 #    hard-wired to LEDs on the FPGA. replace any register with $r24 to see its value mid run.
+# NOTE: $r24 is only hard-wired in main branch. please merge changes before doing stuff.
 # BTNC is the reset button (always). If BTNC does not reset then life sucks.
 #
 #####
