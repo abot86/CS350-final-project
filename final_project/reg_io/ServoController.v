0b82f365fbde673bdfc95ec528e41fca5eea1fc9
ishansvyas <120070801+ishansvyas@users.noreply.github.com>
2024-12-09 13:28:28 -0500

    servocontroller - CORRECT NUMBERS

--- final_project/reg_io/ServoController.v
+++ final_project/reg_io/ServoController.v
@@ -22,40 +22,45 @@ module ServoController(clk25mhz, reset, r2case, servoSignal, testing);
 
     always @(posedge clk25mhz) begin
         if (reset != 0) begin
-            duty_cycle_input_reg <= 77;
+            duty_cycle_input_reg <= 45;
             counter <= 0;
         end
         else if (r2case_reg == 0) begin
-            if (counter >= 40000000) begin
-                duty_cycle_input_reg <= 77;
+            if (counter >= 10000000) begin
+                duty_cycle_input_reg <= 45;
                 treg <= 1;
             end
             else begin
-                duty_cycle_input_reg <= 35;
+                duty_cycle_input_reg <= 20;
                 counter <= counter + 1;
                 treg <= 2;
             end
         end
         else if (r2case_reg == 1) begin
-            if (counter >= 40000000) begin
-                duty_cycle_input_reg <= 77;
+            if (counter >= 10000000) begin
+                duty_cycle_input_reg <= 45;
                 treg <= 3;
             end
             else begin
-                duty_cycle_input_reg <= 108;
+                duty_cycle_input_reg <= 70;
                 counter <= counter + 1;
                 treg <= 4;
             end
         end
         else if (r2case_reg == 2) begin
             counter <= 0;
-            duty_cycle_input_reg <= 108;
+            duty_cycle_input_reg <= 70;
             treg <= 5;
         end
         else if (r2case_reg == 3) begin
             counter <= 0;
-            duty_cycle_input_reg <= 35;
+            duty_cycle_input_reg <= 20;
             treg <= 6;
+        end
+        // LAST CASE FOR TESTING ONLY
+        else if (r2case_reg == 5) begin
+            duty_cycle_input_reg <= 45;
+            treg <= 7;
         end
     end
 
